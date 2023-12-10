a9bdc649aad1c6e09d382de761598704808d18c9
butch0012 <elnathanmburu@gmail.com>
2023-12-10 17:26:23 +0300

    Handle pm times (except 12 pm) by adding 12 to the hour

--- ConvertingTime.py
+++ ConvertingTime.py
@@ -1,6 +1,10 @@
 def convert_to_24_hour(hour, minute, period):
     # changes the hour based on the period (am/pm)
-     if period == "am" and hour == 12:
+    if period == "am" and hour == 12:
         # 12 am is to be converted to 00
         hour = 0
-         
+    elif period == "pm" and hour != 12:
+        # For pm (except 12 pm), add 12 to the hour
+        hour += 12
+    # Formating the time to a four-digit string with leading zeros
+    return f"{hour:02d}{minute:02d}"
