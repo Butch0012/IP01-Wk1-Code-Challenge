12db7136f8c588bd2931be54aa34b1a90bf23e40
butch0012 <elnathanmburu@gmail.com>
2023-12-10 17:20:20 +0300

    Initial implementation of convert_to_24_hour function

--- ConvertingTime.py
+++ ConvertingTime.py
@@ -1,17 +1,2 @@
-#Converting 12-hour time to 24-hour time
 def convert_to_24_hour(hour, minute, period):
     # changes the hour based on the period (am/pm)
-    if period == "am" and hour == 12:
-        # 12 am is to be   converted to 00
-        hour = 0
-    elif period == "pm" and hour != 12:
-        # For pm  (except 12 pm), add 12 to the hour
-        hour += 12
-
-    # Formating the time to a four-digit string with leading zeros
-    return f"{hour:02d}{minute:02d}"
-
-#usage:
-result1 = convert_to_24_hour(8, 30, "am")
-print("Converted time :", result1)  
-# Expected Output: Converted time 1: 0830
