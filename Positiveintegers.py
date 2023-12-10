54d89765873d153f0b48e543824fc29c4e9fcd91
butch0012 <elnathanmburu@gmail.com>
2023-12-10 17:49:17 +0300

    Return True if exactly two numbers are positive

--- Positiveintegers.py
+++ Positiveintegers.py
@@ -1,3 +1,5 @@
 def two_positive_numbers(a, b, c):
     # Count the number of positive numbers using a list comprehension
     positive_count = sum(x > 0 for x in [a, b, c])
+ # Return True if exactly two numbers are positive, False otherwise
+    return positive_count == 2
