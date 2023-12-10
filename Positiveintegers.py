bba30355f21c3edc5b4ba4ac1009782742a781a7
butch0012 <elnathanmburu@gmail.com>
2023-12-10 17:51:21 +0300

    Update examples in comments for clarity

--- Positiveintegers.py
+++ Positiveintegers.py
@@ -3,3 +3,5 @@ def two_positive_numbers(a, b, c):
     positive_count = sum(x > 0 for x in [a, b, c])
  # Return True if exactly two numbers are positive, False otherwise
     return positive_count == 2
+# Expected output:
+print("Result 1:", two_positive_numbers(4, 8, -3)) #True
