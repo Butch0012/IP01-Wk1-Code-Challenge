a91cd26d9ba937ed6471238a57363852df5b28fe
butch0012 <elnathanmburu@gmail.com>
2023-12-10 17:21:37 +0300

    Handle 12 am case by converting it to 00

--- ConvertingTime.py
+++ ConvertingTime.py
@@ -1,2 +1,5 @@
 def convert_to_24_hour(hour, minute, period):
     # changes the hour based on the period (am/pm)
+     if period == "am" and hour == 12:
+        # 12 am is to be converted to 00
+        hour = 0
