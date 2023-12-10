4b4d96acf781e33a6939d18bb44cc91a8f303230
butch0012 <elnathanmburu@gmail.com>
2023-12-10 18:01:45 +0300

    Calculate values for each consonant substring

--- Consonant.py
+++ Consonant.py
@@ -3,3 +3,5 @@ def solve(word):
     consonant_values = []
     # Loop  the characters of the word
     for substring in word.split('aeiou'):
+        # Initializing a list to store individual character values
+        values = [ord(char) - ord('a') + 1 for char in substring]
